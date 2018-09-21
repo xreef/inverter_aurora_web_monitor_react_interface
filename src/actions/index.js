db8f4c09a150a5230f00c16f9fb9a5ada6d86d20
Renzo <renzo.mischianti@marketwall.com>
2018-09-21 22:24:06 +0200

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by renzo on 17/05/2017.
+ */
+
+import {getVersion} from 'version'
+
+export {
+    getVersion
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import version from './version';
+
+const reducersAurora = combineReducers({
+    version
+});
+
+export default reducersAurora

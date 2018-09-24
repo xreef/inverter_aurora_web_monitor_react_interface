4de887adaf5db72c45e9f9999c893fca0c7a1072
renzo.mischianti@gmail.com <cicciolo77j>
2018-09-24 10:55:00 +0200

    stockchart added

---
+++ src/containers/ChartExamplesContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ChartExamples from "./chartExamples/index"
+
+const ChartExamplesContainer = () => (
+    <div>
+        <ChartExamples></ChartExamples>
+    </div>
+);
+
+export default ChartExamplesContainer;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import { Home, About } from './containers';
 import GridShowCaseContainer from "./containers/GridShowCaseContainer";
+import ChartExamplesContainer from "./containers/ChartExamplesContainer";
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/grid_showcase" component={GridShowCaseContainer} />
+        <Route path="/chart_examples" component={GridShowCaseContainer} />
     </Switch>
 );
 
